4c46b9783648d31bd5563eee89dcafa5fc8d1a6c
Rahul R <85353906+rahul83941@users.noreply.github.com>
2024-04-24 20:26:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.get('/products/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
